bcc8c19787afa3d958184f9cef4adbbd3338bdf1
Rigel David <it@promosergrupo.com>
2021-12-23 13:16:23 -0600

    Building the cookie in auth.py

--- core/security/auth.py
+++ core/security/auth.py
@@ -1,11 +1,55 @@
+from datetime import timedelta
+from fastapi import Depends, APIRouter, Response
+from fastapi.security import OAuth2PasswordRequestForm
+from fastapi_login.exceptions import InvalidCredentialsException
 from fastapi_login import LoginManager
 from decouple import config
+from starlette import responses
+
+from .tools import verify_password
+from ..database import db_user
 
 
-AUTH_SECRET_KEY = config("AUTH_SCRET_KEY")
-
-manager = LoginManager(AUTH_SECRET_KEY, '/')
+AUTH_SECRET_KEY = config("AUTH_SECRET_KEY")
 
 
+router = APIRouter(
+    prefix='/auth',
+    tags=['auth']
+)
 
+
+manager = LoginManager(
+    AUTH_SECRET_KEY, 
+    '/auth', 
+    use_cookie=True,
+    use_header=False,
+    default_expiry=timedelta(hours=12))
+
+
+@manager.user_loader()
+async def user_exist(key: str):
+    user = db_user.get(key)
+    return user
+
+
+@router.post('/', response_description="Login form route.")
+async def login(response: Response, data: OAuth2PasswordRequestForm = Depends()):
+    username = data.username
+    password = data.password
+
+    user = await user_exist(username)
+
+    if not user:
+        raise InvalidCredentialsException
+    elif verify_password(password, user["password"]) == False:
+        raise InvalidCredentialsException
+    
+    access_token = manager.create_access_token(
+        data=dict(sub=user["key"])
+    )
+
+    manager.set_cookie(response, access_token)
+
+    return {'token': access_token}
 
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 
+from core.security import auth
 from core import bussiness
 from core import users
 
@@ -12,6 +13,5 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 app.include_router(bussiness.router)
 app.include_router(users.router)
-
-
+app.include_router(auth.router)
 
